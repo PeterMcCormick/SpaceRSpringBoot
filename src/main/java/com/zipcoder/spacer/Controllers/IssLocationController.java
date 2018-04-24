a16c9a2d6a5bd0c7f31a3a77c364d007ebe11dc0
PeterMcCormick <mcpeter@udel.edu>
2018-04-24 16:36:12 -0400

    IssLocationService Updated but not done

--- src/main/java/com/zipcoder/spacer/Controllers/IssLocationController.java
+++ src/main/java/com/zipcoder/spacer/Controllers/IssLocationController.java
@@ -1,6 +1,17 @@
 package com.zipcoder.spacer.Controllers;
 
+import com.zipcoder.spacer.Services.IssLocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IssLocationController {
 
+    private IssLocationService issLocationService;
+
+    @Autowired
+    public IssLocationController(IssLocationService issLocationService) {
+        this.issLocationService = issLocationService;
+    }
 
 }
--- src/main/java/com/zipcoder/spacer/Services/IssLocationService.java
+++ src/main/java/com/zipcoder/spacer/Services/IssLocationService.java
@@ -1,4 +1,25 @@
 package com.zipcoder.spacer.Services;
 
+import com.zipcoder.spacer.Domain.IssLocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
+
+@Service
 public class IssLocationService {
+
+
+
+    @Autowired
+    public IssLocationService() {
+
+    }
+
+    public ResponseEntity<IssLocation> getIssLocation() {
+        IssLocation issLocation = issLocationRepository;
+        return new ResponseEntity<>(issLocation, HttpStatus.OK);
+    }
 }
