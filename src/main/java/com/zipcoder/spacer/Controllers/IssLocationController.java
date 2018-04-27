925896a8efe3114ea63e93f563fd9b76c0afd8f7
Anthony Jordan <Kain235@gmail.com>
2018-04-27 13:27:01 -0400

    Added issLocation logic

--- src/main/java/com/zipcoder/spacer/Controllers/IssLocationController.java
+++ src/main/java/com/zipcoder/spacer/Controllers/IssLocationController.java
@@ -3,6 +3,7 @@ package com.zipcoder.spacer.Controllers;
 import com.zipcoder.spacer.Services.IssLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class IssLocationController {
         this.issLocationService = issLocationService;
     }
 
+    @CrossOrigin
     @RequestMapping(value= "/iss_location", method = RequestMethod.GET)
     public ResponseEntity getIssLocationFromDataBase() {
         return this.issLocationService.getIssLocationFromDB();
--- src/main/java/com/zipcoder/spacer/Services/IssLocationService.java
+++ src/main/java/com/zipcoder/spacer/Services/IssLocationService.java
@@ -1,29 +1,42 @@
 package com.zipcoder.spacer.Services;
 
+
 import com.zipcoder.spacer.Domain.IssLocation;
 import com.zipcoder.spacer.Repositories.IssLocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import java.io.IOException;
 
 
 @Service
 public class IssLocationService {
 
     private IssLocationRepository issLocationRepository;
+    private RestTemplate restTemplate = new RestTemplate();
 
     @Autowired
     IssLocationService(IssLocationRepository issLocationRepository) {
         this.issLocationRepository = issLocationRepository;
     }
 
-    @RequestMapping(value= "/isslocation", method = RequestMethod.POST)
-    public void getIssLocationFromAPI(@RequestBody IssLocation issLocationToUpdate) {
-        this.issLocationRepository.save(issLocationToUpdate);
+    @Scheduled(fixedDelay = 60000)
+    public void getIssLocationFromAPI() {
+        HttpEntity entity = new HttpEntity<String>("");
+        ResponseEntity<String> responseEntity = restTemplate.exchange("http://api.open-notify.org/iss-now.json",
+                HttpMethod.GET, entity , String.class);
+        try {
+            IssLocation issLocation = new IssLocation(responseEntity.getBody());
+            this.issLocationRepository.deleteAll();
+            this.issLocationRepository.save(issLocation);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public ResponseEntity getIssLocationFromDB() {
